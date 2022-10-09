27410674229ab989bb7808b69a85d9eca461fcce
Maksim <mc0808@gmail.com>
2022-10-09 22:24:36 +0300

    новый файл html и код в  script js

--- script.js
+++ script.js
@@ -8,12 +8,31 @@ const personalMovisDB = {
 
 };
 
-const a = prompt('Один из просмотренных фильмов', '');
-      b = prompt('На сколько оцените его?', '');
-      c = prompt('Один из просмотренных фильмов', '');
-      d = prompt('На сколько оцените его?', '');
+      
+      
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из просмотренных фильмов', '');
+          b = prompt('На сколько оцените его?', '');
 
-      personalMovisDB.movies[a] = b;
-      personalMovisDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovisDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+    if(personalMovisDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovisDB.count >= 10 && personalMovisDB.count < 30 ) {
+        console.log("Вы обычный зритель");
+    } else if (personalMovisDB.count >= 30) {
+        console.log("Вы кино фанат");
+    } else {
+        console.log(" Произошла ошибка");
+    }
+          
+}
+
 
 console.log(personalMovisDB);
